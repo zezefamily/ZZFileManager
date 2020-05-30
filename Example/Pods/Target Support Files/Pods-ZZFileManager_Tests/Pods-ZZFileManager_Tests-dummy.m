f58528aa0d061d49540f2c85e23b83fa9e55b352
wuwenmei <wuwenmei@100tal.com>
2020-05-30 23:13:28 +0800

    磁盘文件管理器

---
+++ Example/Pods/Target Support Files/Pods-ZZFileManager_Tests/Pods-ZZFileManager_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ZZFileManager_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_ZZFileManager_Tests
+@end
---
+++ Example/Pods/Target Support Files/ZZFileManager/ZZFileManager-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ZZFileManager : NSObject
+@end
+@implementation PodsDummy_ZZFileManager
+@end
